3b5b2e953f6040395e67fe81aff9c4de05c4b71b
Sena <joaosenadainese@gmail.com>
2026-02-05 23:38:53 -0300

    Adicionar bin e obj in gitignore

--- src/Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
+++ src/Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04aff3d3a8affa2f1e178ea61c538ccaa0e9631d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71dcdaa0d548db1ba511c844e8d02194467920a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
