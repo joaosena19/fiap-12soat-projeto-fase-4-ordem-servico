80723012fa664f20516d9e5f36129cf46e9c6de6
Sena <joaosenadainese@gmail.com>
2026-02-15 13:48:26 -0300

    Ajuste bucket terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "fiap-12soat-fase3-joao-dainese"
+    bucket  = "fiap-12soat-fase4-joao-dainese"
     key     = "fase4/ordem-servico-database/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
